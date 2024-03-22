06567b4543365db9f39ce188213dd66ce5a1508e
Vasco van Gils <vascovangils@gmail.com>
2024-03-22 14:16:57 +0100

    nav responsive

--- Code/main.js
+++ Code/main.js
@@ -1,22 +1,26 @@
-
 function validateEmail() {
   var emailInput = document.getElementById("email");
-  var emailError = "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcrmbo.nl.";
+  var emailError =
+    "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcrmbo.nl.";
   var allowedDomains = ["student.zadkine.nl", "tcrmbo.nl"];
 
   var email = emailInput.value.trim().toLowerCase();
   var domain = email.split("@")[1];
 
   if (allowedDomains.indexOf(domain) === -1) {
-    emailError =  alert("Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcrmbo.nl.");
-     return false;
+    emailError = alert(
+      "Gebruik een geldig emailadres met het domein @student.zadkine.nl of @tcrmbo.nl."
+    );
+    return false;
   } else {
-
-     return true;
+    return true;
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggle = document.getElementById("navbar-toggle");
+  const navbarItems = document.getElementById("navbar-items");
 
-
-
-
-
+  navbarToggle.addEventListener("click", function () {
+    navbarItems.classList.toggle("active");
+  });
+});
