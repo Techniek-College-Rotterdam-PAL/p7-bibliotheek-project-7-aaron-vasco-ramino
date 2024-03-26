c66430167287653150bd2a7555fdabc47b722ce5
Vasco van Gils <vascovangils@gmail.com>
2024-03-26 13:41:49 +0100

    Reserve books.php

--- Code/student_reserved.php
+++ Code/student_reserved.php
@@ -15,34 +15,35 @@
     include 'session_function.php';
     require_once 'db.php';
 
-    class Reservedbooks{
+    class Reservedbooks
+    {
         private $conn;
-          
-        public function __construct($conn){
-            $this->conn = $conn; 
+
+        public function __construct($conn)
+        {
+            $this->conn = $conn;
         }
-    
-        public function Selectreservedbooks(){
+
+        public function Selectreservedbooks()
+        {
             $query = "SELECT * FROM reserveren";
             $stmt = $this->conn->prepare($query);
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
     }
-    
-   
+
+
     $reservedbooks = new Reservedbooks($conn);
     $reservedBooksArray = $reservedbooks->Selectreservedbooks();
-    
+
     foreach ($reservedBooksArray as $reservedbook) {
 
 
-        echo "<div> Voornaam: " . $reservedbook['voornaam'] . " Achternaam: " . $reservedbook['achternaam'] . " Titel: " . $reservedbook['titel'] . " Time: " . $reservedbook['time'] . "</div>";
-    
-
+        echo "<div class='student_reserved'> Voornaam: " . $reservedbook['voornaam'] . " Achternaam: " . $reservedbook['achternaam'] . " Titel: " . $reservedbook['titel'] . " Time: " . $reservedbook['time'] .
+            "<p> </p>" .    '<button type="submit" class="search-button-reserve" name="submit">Click Me!</button>' . "</div>";
     }
-      
-
+    require 'footer.php'
     ?>
 
 </body>
