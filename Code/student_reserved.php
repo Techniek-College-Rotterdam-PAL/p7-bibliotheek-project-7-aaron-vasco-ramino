4ce3e43e769789065c5ed099b3c1abda5cda6590
Firey-event559 <RaminoHiromiVrca@gmail.com>
2024-03-26 13:00:10 +0100

    update

--- Code/student_reserved.php
+++ Code/student_reserved.php
@@ -13,6 +13,43 @@
     <?php
     include 'nav_docent.php';
     include 'session_function.php';
+    require_once 'db.php';
+
+    class Reservedbooks{
+        private $conn;
+          
+        public function __construct($conn){
+            $this->conn = $conn; 
+        }
+    
+        public function Selectreservedbooks(){
+            $query = "SELECT * FROM reserveren";
+            $stmt = $this->conn->prepare($query);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+    
+   
+    $reservedbooks = new Reservedbooks($conn);
+    $reservedBooksArray = $reservedbooks->Selectreservedbooks();
+    
+    foreach ($reservedBooksArray as $reservedbook) {
+
+        
+        echo "<div> Voornaam: " . $reservedbook['voornaam'] . "</div><div> Achternaam: " .  $reservedbook['achternaam'] . "</div>" .  
+             "<div>" . $reservedbook['titel'] . "</div>" .
+             "<div>" . $reservedbook['time'] . "</div>";
+    }
+    
+
+    
+    
+
+
+
+    
+    
 
     ?>
 
