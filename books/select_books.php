9a3052b25595d6af555f9c8807a62f50bd102ad3
Vasco van Gils <vascovangils@gmail.com>
2024-04-04 10:24:08 +0200

    boeken styling

--- books/select_books.php
+++ books/select_books.php
@@ -1,13 +1,16 @@
 <?php
 
-class Books {
+class Books
+{
     private $conn;
 
-    public function __construct($conn) {
+    public function __construct($conn)
+    {
         $this->conn = $conn;
     }
 
-    public function Selectallbooks() {
+    public function Selectallbooks()
+    {
         $query = "SELECT * FROM boeken WHERE beschikbaarheid = 1";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
@@ -19,29 +22,35 @@ $booksmanager = new Books($conn);
 $books = $booksmanager->Selectallbooks();
 
 foreach ($books as $book) {
-    echo "<div class='book_container'>" .  
-            "<div class='books'>" .  
-                    
-                    $book['titel']  .   
-                 "<div>ISBN: "  .   $book['isbn'] . "</div>"  . "<br>" .
-                    $book['schrijver'] . "<br>" .
-                    $book['uitgever'] .  "<br>" .
-                    $book['boekjaar'] .  "<br>";
+    echo "<div class='book_container'>" .
+        "<div class='select_books'>" .
+
+
+
+        $book['titel']  .
+        "<div>ISBN: "  .   $book['isbn'] . "</div>"  . "<br>" .
+        $book['schrijver'] . "<br>" .
+        $book['uitgever'] .  "<br>" .
+        $book['boekjaar'] .  "<br>";
+    echo $book['informatie_boek'];
+    echo "<div class='voorraad'>Voorraad: " . $book['voorraad'] . "</div>";
+
+
+
 
     if (!empty($book['img'])) {
         $imagepath = $book['img'];
-        echo '<img src="../Code/upload/' . $imagepath . '" width="100" height="100" class="book_image" alt="img_book" ><br>';
+        echo '<img src="../Code/upload/' . $imagepath . '"class="book_image" height="180px" width="160px "alt="img_book" ><br>';
     }
-
-    echo $book['informatie_boek'];
-    echo "<div>Voorraad: " . $book['voorraad'] . "</div>";
-
-
     echo "<form method='post' action='../reserve/reserve_books.php'>";
     echo "<button class='reserve' name='submit' value='" . $book['id']  . "' class=''>reserveren</button>"
 
-     . "</form>";
+        . "</form>";
 
 
-    echo "</div></div></div></div>"; 
+
+
+
+
+    echo "</div></div></div></div>";
 }
