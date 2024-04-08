0bcbda6e5738b3ee5e4a47fcfd5d5c3d7d33e239
Aaron24115 <9018817@student.zadkine.nl>
2024-04-08 10:50:44 +0200

    update

--- Code/docent.php
+++ Code/docent.php
@@ -16,11 +16,16 @@ include '../account/session_function_docent.php';
 
 ?>
 
+
     <h2 class="main-page">Docenten pagina</h2>
-    <p class="main-page">
+    <div class="teacher-image">
+    <img src="../Images/teacher-explains.jpg" alt="foto van een docent" width="700pxu" height="500px">
+    </div><br>
+    <p class="teacher-page">
         welkom bij de docenten pagina, hier vindt jij een <br>
         overzicht van alle rechten die je als docent hebt op deze<br>
         website.
+</p>
 
     <?php
     include 'footer.php'; ?>
